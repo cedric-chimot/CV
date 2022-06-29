e0fbb198ca17d70cdf6b20682abe524469fc6d58
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2022-06-29 16:00:07 +0200

    css formulaire CV

--- CV/Formulaire/form_contact.js
+++ CV/Formulaire/form_contact.js
@@ -28,5 +28,48 @@ function valider()
             document.getElementById("erreur_nom").innerHTML = "";
         }
 
+    if(email.value == "") {
 
+        document.getElementById("erreur_mail").innerHTML = "Veuillez entrer une adresse mail valide";
+        email.focus();
+        return false;
+
+    }
+
+    else {
+        document.getElementById("erreur_mail").innerHTML = "";
+    }
+
+    if(email.value.indexOf("@", 0) < 0) {
+        document.getElementById("erreur_mail").innerHTML = "Veuillez entrer une adresse mail valide";
+        email.focus();
+        return false;
+    }
+
+    if(email.value.indexOf(".", 0) < 0) {
+        document.getElementById("erreur_mail").innerHTML = "Veuillez entrer une adresse mail valide";
+        email.focus();
+        return false;
+    }
+
+    if(entreprise.value == "") {
+
+        document.getElementById("erreur_entr").innerHTML = "Veuillez entrer un nom d'entreprise valide";
+        entreprise.focus();
+        return false;
+        }
+    
+    else {
+        document.getElementById("erreur_entr").innerHTML = "";
+    }
+       
+    if(message.value == "") {
+        document.getElementById("erreur_message").innerHTML = "Veuillez entrer un un message";
+        message.focus();
+        return false;
+    }
+    
+    else {
+        document.getElementById("erreur_message").innerHTML = "";
+    }
 }
