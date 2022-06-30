0b9c1e3b1130bd27ce8a4e2232f9ce9f3f481414
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2022-06-30 22:08:29 +0200

    finalisation formulaire + CV

--- CV/CV2/formulaire/form_contact.js
+++ CV/CV2/formulaire/form_contact.js
@@ -1,52 +1,54 @@
-function valider()
-{
+function Valider() {
+
     var nom = document.forms["contact"]["nom"];
     var prenom = document.forms["contact"]["prenom"];
     var email = document.forms["contact"]["email"];
     var entreprise = document.forms["contact"]["entreprise"];
     var message = document.forms["contact"]["message"];
 
-    if (nom.value == "")                                  
+    if (nom.value == "")                               
     { 
-        alert("Mettez votre nom."); 
+        alert("Veuillez entrer un nom valide !"); 
         nom.focus(); 
         return false; 
-    }    
+    }
+
     if (prenom.value == "")                               
     { 
-        alert("Mettez votre adresse."); 
+        alert("Veuillez entrer un prénom valide !"); 
         prenom.focus(); 
         return false; 
-    }        
+    }      
     if (email.value == "")                                   
     { 
-        alert("Mettez une adresse email valide."); 
+        alert("Veuillez entrer une adresse email valide !"); 
         email.focus(); 
         return false; 
     }    
     if (email.value.indexOf("@", 0) < 0)                 
     { 
-        alert("Mettez une adresse email valide."); 
+        alert("Veuillez entrer une adresse email valide !"); 
         email.focus(); 
         return false; 
     }    
     if (email.value.indexOf(".", 0) < 0)                 
     { 
-        alert("Mettez une adresse email valide."); 
+        alert("Veuillez entrer une adresse email valide !"); 
         email.focus(); 
         return false; 
     }    
     if (entreprise.value == "")                           
     { 
-        alert("Mettez votre numéro de téléphone."); 
+        alert("Veuillez entrer un nom d'entreprise valide !"); 
         entreprise.focus(); 
         return false; 
     }    
     if (message.value == "")                  
     { 
-        alert("Écrivez un commentaire."); 
+        alert("Veuillez écrire votre message !"); 
         message.focus(); 
         return false; 
     }
 
+    return alert("Merci pour votre message, je vous répondrai dés que possible !");
 }
